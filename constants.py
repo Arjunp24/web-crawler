46c0010302b8024d59129da645d6e2463192d226
Arjun Prashanth <arjun.prashant@gmail.com>
2020-05-31 23:09:53 -0400

    added web_pages_to_crawl constants.py

--- constants.py
+++ constants.py
@@ -17,3 +17,5 @@ KEYWORDS = ["obama", "family", "michelle", "sasha"]
 URL_KEYWORD_SCORE_DICT = {"obama": 100, "family": 50, "michelle": 20, "sasha": 20, "barack": 30}
 
 ANCHOR_TEXT_KEYWORD_DICT = {"obama": 200, "family": 110, "michelle": 50, "sasha": 50, "barack": 60}
+
+WEB_PAGES_TO_CRAWL = 40000
